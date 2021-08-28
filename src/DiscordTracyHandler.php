09b95a27e7ee3fb4fc2dbc368b77d8639c5530fc
Rixafy <rixafy@gmail.com>
2021-08-29 00:08:12 +0200

    Send file when log is created

--- src/DiscordTracyHandler.php
+++ src/DiscordTracyHandler.php
@@ -5,57 +5,45 @@ declare(strict_types=1);
 namespace Rixafy\DiscordTracy;
 
 use DiscordHandler\DiscordHandler;
-use Psr\Log\LogLevel;
+use GuzzleHttp\Exception\RequestException;
 use Throwable;
 use Tracy\Debugger;
 use Tracy\Logger;
 
 class DiscordTracyHandler extends DiscordHandler
 {
-    private array $records = [];
+	protected function write(array $record): void
+	{
+		try {
+			parent::write($record);
 
-    protected function write(array $record): void
-    {
-        parent::write($record);
-        $this->records[] = $record;
-    }
+			$logger = new Logger(Debugger::$logDirectory);
+			$exception = $record['context']['exception'] ?? null;
+			if ($exception instanceof Throwable) {
+				$file = $logger->getExceptionFile($record['context']['exception'], strtolower($record['level_name']));
+				if (!file_exists($file)) {
+					Debugger::getBlueScreen()->renderToFile($exception, $file);
+				}
 
-    public function __destruct()
-    {
-        $logger = new Logger(Debugger::$logDirectory);
-        
-        foreach ($this->records as $record) {
-            $exception = $record['context']['exception'] ?? null;
-            if ($exception instanceof Throwable) {
-                $level = constant(LogLevel::class . '::' . strtoupper($record['level_name']));
-                if ($level === null) {
-                    return;
-                }
-
-                $file = $logger->getExceptionFile($record['context']['exception'], $level);
-                if (!file_exists($file)) {
-                    return;
-                }
-                
-                $fileName = basename($file);
-                foreach ($this->config->getWebHooks() as $webHook) {
-                    $curl = curl_init($webHook);
-                    curl_setopt($curl, CURLOPT_TIMEOUT, 3);
-                    curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 3);
-                    curl_setopt($curl, CURLOPT_POST, 1);
-                    curl_setopt($curl, CURLOPT_HTTPHEADER, [
-                        'Content-Type: multipart/form-data'
-                    ]);
-                    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-                    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-                    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-                    curl_setopt($curl, CURLOPT_POSTFIELDS, [
-                        'file' => curl_file_create($file, 'text/html', $fileName)
-                    ]);
-                    curl_exec($curl);
-                    curl_close($curl);
-                }
-            }
-        }
-    }
+				$fileName = basename($file);
+				foreach ($this->config->getWebHooks() as $webHook) {
+					$curl = curl_init($webHook);
+					curl_setopt($curl, CURLOPT_TIMEOUT, 3);
+					curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 3);
+					curl_setopt($curl, CURLOPT_POST, 1);
+					curl_setopt($curl, CURLOPT_HTTPHEADER, [
+						'Content-Type: multipart/form-data'
+					]);
+					curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+					curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+					curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+					curl_setopt($curl, CURLOPT_POSTFIELDS, [
+						'file' => curl_file_create($file, 'text/html', $fileName)
+					]);
+					curl_exec($curl);
+					curl_close($curl);
+				}
+			}
+		} catch (RequestException $e) {}
+	}
 }
